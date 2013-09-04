4fad55e84517babf93d81cbcaf6fe60b081dafec
niklebedev <Nikolay.Lebedev@aplana.com>
2013-09-04 16:43:20 +0600

    APLANATS-950

--- src/main/java/com/aplana/timesheet/dao/EmployeeReportDAO.java
+++ src/main/java/com/aplana/timesheet/dao/EmployeeReportDAO.java
@@ -1,5 +1,6 @@
 package com.aplana.timesheet.dao;
 
+import com.aplana.timesheet.enums.TypesOfActivityEnum;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -17,7 +18,11 @@ public class EmployeeReportDAO {
 
     /* получаем список типов активности и суммы времени по каждому*/
     public List<Object[]> getEmployeeMonthData(Integer employee_id, Integer year, Integer month) {
-        final Query query = entityManager.createQuery(
+        List<Integer> projectAndProjectPresale = new ArrayList<Integer>();
+        projectAndProjectPresale.add(TypesOfActivityEnum.PROJECT.getId());
+        projectAndProjectPresale.add(TypesOfActivityEnum.PROJECT_PRESALE.getId());
+
+        final Query queryPPS = entityManager.createQuery(
                 " select min(ac)," +
                 "        p," +
                 "        sum(tsd.duration)" +
@@ -28,10 +33,36 @@ public class EmployeeReportDAO {
                 " left outer join tsd.project p" +
                 " where c.year = :year and c.month = :month" +
                 " and ts.employee.id = :employee" +
-                " group by p.id" +
-                " order by 1 asc, 2 desc"
-        ).setParameter("year", year).setParameter("month", month).setParameter("employee", employee_id);
+                " and ac.id in :activityType" +
+                " group by p" +
+                " order by 1 asc, p.name asc")
+                .setParameter("year", year)
+                .setParameter("month", month)
+                .setParameter("employee", employee_id)
+                .setParameter("activityType", projectAndProjectPresale);
 
-        return query.getResultList();
+        final Query queryOther = entityManager.createQuery(
+                " select ac," +
+                "        p," +
+                "        sum(tsd.duration)" +
+                " from Calendar c" +
+                " inner join c.timeSheets ts" +
+                " inner join ts.timeSheetDetails tsd" +
+                " inner join tsd.actType ac" +
+                " left outer join tsd.project p" +
+                " where c.year = :year and c.month = :month" +
+                " and ts.employee.id = :employee" +
+                " and ac.id not in :activityType" +
+                " group by ac, p" +
+                " order by ac asc, p.name asc")
+                .setParameter("year", year)
+                .setParameter("month", month)
+                .setParameter("employee", employee_id)
+                .setParameter("activityType", projectAndProjectPresale);
+
+        List<Object[]> result = new ArrayList<Object[]>();
+        result.addAll(queryPPS.getResultList());
+        result.addAll(queryOther.getResultList());
+        return result;
     }
 }
