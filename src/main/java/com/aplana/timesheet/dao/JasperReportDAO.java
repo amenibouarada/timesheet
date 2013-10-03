6635a082efcd9a217574d74d02a947faf0e2c1d1
Alexander Ivanov <alivanov@aplana.com>
2013-10-03 17:46:29 +0600

    APLANATS-1343 доработка, тип активности только пресейлы, проектные пресейлы и проект

--- src/main/java/com/aplana/timesheet/dao/JasperReportDAO.java
+++ src/main/java/com/aplana/timesheet/dao/JasperReportDAO.java
@@ -378,6 +378,7 @@ public class JasperReportDAO {
                                 "%s " +
                         "WHERE " +
                                 "timesheet_details.duration > 0 AND " +
+                                "timesheet_details.act_type in (42, 12, 13)  AND " +    //TODO (aivanov 3.10.13) Создать емун и заменить циферки
                                 " %s %s %s %s %s " +
                                 "calendar.calDate between :beginDate AND :endDate " +
                         "GROUP BY " +
@@ -533,6 +534,7 @@ public class JasperReportDAO {
                     "%s " +
             "WHERE " +
                     "timesheet_details.duration > 0 AND " +
+                    "timesheet_details.act_type in (42, 12, 13)  AND " +    //TODO (aivanov 3.10.13) Создать емун и заменить циферки
                     " %s %s %s %s %s " +
                     "calendar.caldate between :beginDate AND :endDate " +
             "GROUP BY " +
