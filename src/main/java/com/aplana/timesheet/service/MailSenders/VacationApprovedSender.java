89b2644d00f354d186f1f22da09baf79a7017bf1
Pmakarov <Pavel.Makarov@aplana.com>
2013-10-09 13:54:56 +0600

    APLANATS-1124: Текст заголовка письма

--- src/main/java/com/aplana/timesheet/service/MailSenders/VacationApprovedSender.java
+++ src/main/java/com/aplana/timesheet/service/MailSenders/VacationApprovedSender.java
@@ -21,12 +21,15 @@ import java.util.List;
 /**
  * User: vsergeev
  * Date: 13.02.13
+ * Письмо отправляется, когда отпуск согласован полностью.
  */
 public class VacationApprovedSender extends AbstractVacationSenderWithCopyToAuthor {
 
     protected static final Logger logger = LoggerFactory.getLogger(VacationApprovedSender.class);
 
     private final List<String> emails;
+    final String MAIL_ACCEPT_SUBJECT = "Утвержден отпуск %s - %s";
+    final String MAIL_REFUSE_SUBJECT = "Отклонен отпуск %s - %s";
 
     public VacationApprovedSender(SendMailService sendMailService, TSPropertyProvider propertyProvider,
                                   List<String> emails) {
@@ -118,9 +121,7 @@ public class VacationApprovedSender extends AbstractVacationSenderWithCopyToAuth
     private String getSubject(Vacation vacation, Boolean accepted) {
         String beginDateStr = DateFormatUtils.format(vacation.getBeginDate(), DATE_FORMAT);
         String endDateStr = DateFormatUtils.format(vacation.getEndDate(), DATE_FORMAT);
-        return  String.format(accepted?"Согласован отпуск %s - %s":"Отклонен отпуск %s - %s", beginDateStr, endDateStr);
-//        return  String.format("Согласование %s сотрудника %s на период с %s - %s", vacation.getType().getValue(), vacation.getEmployee().getName(),
-//                        beginDateStr, endDateStr);
+        return  String.format(accepted?MAIL_ACCEPT_SUBJECT:MAIL_REFUSE_SUBJECT, beginDateStr, endDateStr);
     }
 
 }
