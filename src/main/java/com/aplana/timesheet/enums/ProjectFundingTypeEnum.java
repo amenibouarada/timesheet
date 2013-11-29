19c94d6686cd21fc2ed222f0ffa82737c9547a2a
Alexander Ivanov <alivanov@aplana.com>
2013-11-29 16:41:36 +0600

    APLANATS-1506. Доработка синхронизации, добавились новые поля в проекте

--- src/main/java/com/aplana/timesheet/enums/ProjectFundingTypeEnum.java
+++ src/main/java/com/aplana/timesheet/enums/ProjectFundingTypeEnum.java
@@ -9,12 +9,13 @@ import java.util.Arrays;
 Типы источников финансирования проектов
  */
 public enum ProjectFundingTypeEnum implements TSEnum {
-    COMMERCIAL_PROJECT(128, "Из средств заказчика"),
-    INVESTMENT_PROJECT(129, "Инвестиции"),
-    INTERNAL_PROJECT(130, "Внутренние");
+    COMMERCIAL_PROJECT(128, "Из средств заказчика", "10"),
+    INVESTMENT_PROJECT(129, "Инвестиции", "20"),
+    INTERNAL_PROJECT(130, "Внутренние", "30");
 
     private int id;
     private String name;
+    private String fromOQ;
 
     @Override
     public int getId() {
@@ -26,9 +27,14 @@ public enum ProjectFundingTypeEnum implements TSEnum {
         return name;
     }
 
-    private ProjectFundingTypeEnum(int id, String name) {
+    public String getFromOQ() {
+        return fromOQ;
+    }
+
+    private ProjectFundingTypeEnum(int id, String name, String fromOQ) {
         this.id = id;
         this.name = name;
+        this.fromOQ = fromOQ;
     }
 
     public static ProjectFundingTypeEnum getById(final int id) {
@@ -38,5 +44,14 @@ public enum ProjectFundingTypeEnum implements TSEnum {
                 return input.getId() == id;
             }
         });
+    }
+
+    public static ProjectFundingTypeEnum getById(String fromOQ) {
+        for(ProjectFundingTypeEnum item : ProjectFundingTypeEnum.values()){
+            if (item.getFromOQ().equals(fromOQ)){
+                return item;
+            }
+        }
+        throw new IllegalArgumentException("Error in ProjectFundingTypeEnum.getById()");
     }
 }
--- src/main/java/com/aplana/timesheet/enums/TSEnum.java
+++ src/main/java/com/aplana/timesheet/enums/TSEnum.java
@@ -6,5 +6,6 @@ package com.aplana.timesheet.enums;
  */
 public interface TSEnum {
     int getId();
+
     String getName();
 }
