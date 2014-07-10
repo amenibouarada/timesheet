ab64451a03f034a57b8a02569fb98c7e31daabfb
Dmitriy Systerov <demelor@yandex.ru>
2014-07-10 11:47:31 +0600

    http://jira.aplana.com/browse/APLANATS-1757

--- src/main/java/com/aplana/timesheet/dao/LdapDAO.java
+++ src/main/java/com/aplana/timesheet/dao/LdapDAO.java
@@ -1,9 +1,11 @@
 package com.aplana.timesheet.dao;
 
 import com.aplana.timesheet.dao.entity.ldap.EmployeeLdap;
+import com.aplana.timesheet.system.properties.TSPropertyProvider;
 import com.google.common.collect.Iterables;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ldap.NameNotFoundException;
 import org.springframework.ldap.core.AttributesMapper;
 import org.springframework.ldap.core.DistinguishedName;
@@ -26,13 +28,14 @@ import java.util.Map;
 public class LdapDAO {
 	private static final Logger logger = LoggerFactory.getLogger(LdapDAO.class);
 
+    @Autowired
+    private static TSPropertyProvider propertyProvider;
+
 	private LdapTemplate ldapTemplate;
 
-    public static final String SID = "objectSid";
-    public static final String NAME = "description";
-    public static final String LEADER = "managedBy";
-
-
+    public static final String SID = propertyProvider.getLdapFieldForSID();
+    public static final String NAME = propertyProvider.getLdapFieldForDivisionName();
+    public static final String LEADER = propertyProvider.getLdapFieldForLeader();
 
     public void setLdapTemplate(LdapTemplate ldapTemplate) {
 		this.ldapTemplate = ldapTemplate;
@@ -40,9 +43,12 @@ public class LdapDAO {
 
     public EmployeeLdap getEmployeeByEmail(String email) {
         logger.info("Getting Employee {} from LDAP",email);
-        EqualsFilter filter = new EqualsFilter("mail", email);
+        EqualsFilter filter = new EqualsFilter(propertyProvider.getLdapFieldForEmail(), email);
         logger.debug("LDAP Query {}", filter.encode());
-        EmployeeLdap first = (EmployeeLdap) Iterables.getFirst(ldapTemplate.search("", filter.encode(), new EmployeeAttributeMapper()), null);
+        EmployeeLdap first =
+                (EmployeeLdap) Iterables.getFirst(
+                        ldapTemplate.search("", filter.encode(), new EmployeeAttributeMapper()),
+                        null);
         logger.debug("LDAP Query finished. result = {}", first);
         return first;
     }
@@ -50,9 +56,14 @@ public class LdapDAO {
     public EmployeeLdap getEmployeeByLdapName(String name) {
         if (name != null && !name.isEmpty()) {
             try {
-                EqualsFilter filter = new EqualsFilter("distinguishedName", name.replaceAll("/", ","));
+                EqualsFilter filter = new EqualsFilter(
+                        propertyProvider.getLdapFieldForLdapCn(),
+                        name.replaceAll("/", ","));
                 logger.debug("LDAP Query {}", filter.encode());
-                EmployeeLdap first = (EmployeeLdap) Iterables.getFirst(ldapTemplate.search("", filter.encode(), new EmployeeAttributeMapper()), null);
+                EmployeeLdap first =
+                        (EmployeeLdap) Iterables.getFirst(
+                                ldapTemplate.search("", filter.encode(), new EmployeeAttributeMapper()),
+                                null);
                 logger.debug("LDAP Query finished. result = {}", first);
                 return first;
             } catch (NameNotFoundException e) {
@@ -66,9 +77,12 @@ public class LdapDAO {
 
     public EmployeeLdap getEmployeeByDisplayName(String name) {
         try {
-            EqualsFilter filter = new EqualsFilter("displayName", name);
+            EqualsFilter filter = new EqualsFilter(propertyProvider.getLdapFieldForDisplayName(), name);
             logger.debug("LDAP Query {}", filter.encode());
-            EmployeeLdap first = (EmployeeLdap) Iterables.getFirst(ldapTemplate.search("", filter.encode(), new EmployeeAttributeMapper()), null);
+            EmployeeLdap first =
+                    (EmployeeLdap) Iterables.getFirst(
+                            ldapTemplate.search("", filter.encode(), new EmployeeAttributeMapper()),
+                            null);
             logger.debug("LDAP Query finished. result = {}", first);
             return first;
         } catch (NameNotFoundException e) {
@@ -81,7 +95,10 @@ public class LdapDAO {
         try {
             EqualsFilter filter = new EqualsFilter(SID, sid);
             logger.debug("LDAP Query {}", filter.encode());
-            EmployeeLdap first = (EmployeeLdap) Iterables.getFirst(ldapTemplate.search("", filter.encode(), new EmployeeAttributeMapper()), null);
+            EmployeeLdap first =
+                    (EmployeeLdap) Iterables.getFirst(
+                            ldapTemplate.search("", filter.encode(), new EmployeeAttributeMapper()),
+                            null);
             logger.debug("LDAP Query finished. result = {}", first);
             return first;
         } catch (NameNotFoundException e) {
@@ -109,8 +126,10 @@ public class LdapDAO {
 	public List<EmployeeLdap> getEmployees(String department) {
 		logger.info("Getting Employees from LDAP.");
         AndFilter andFilter = new AndFilter()
-                .and(new EqualsFilter("department", department))
-                .and(new EqualsFilter("objectClass", "user"));
+                .and(new EqualsFilter(propertyProvider.getLdapFieldForDivision(), department))
+                .and(new EqualsFilter(
+                        propertyProvider.getLdapFieldForObjectClass(),
+                        propertyProvider.getLdapObjectClassEmployee()));
         logger.debug("LDAP Query {}", andFilter.encode());
 		List<EmployeeLdap> employees = ldapTemplate.search("", andFilter.encode(), new EmployeeAttributeMapper());
 		logger.debug("LDAP Query finished. Employees size is {}", employees.size());
@@ -120,12 +139,15 @@ public class LdapDAO {
 	}
 	
 	@SuppressWarnings("unchecked")
-	public List<EmployeeLdap> getDisabledEmployyes() {
+	public List<EmployeeLdap> getDisabledEmployees() {
 		logger.info("Getting Disabled Employees from LDAP.");
 		DistinguishedName dn = new DistinguishedName();
-	    dn.add("ou", "Disabled Users");
+	    dn.add("ou", propertyProvider.getLdapOuDisabledEmployee());
 
-        AndFilter andFilter = new AndFilter().and(new EqualsFilter("objectClass", "person"));
+        AndFilter andFilter = new AndFilter()
+                .and(new EqualsFilter(
+                        propertyProvider.getLdapFieldForObjectClass(),
+                        propertyProvider.getLdapObjectClassDisabledEmployee()));
 		logger.debug("LDAP Query {}", andFilter.encode());
         List search = ldapTemplate.search(dn, andFilter.encode(), new EmployeeAttributeMapper());
         logger.debug("LDAP Query finished. result.size = {}", search.size());
@@ -136,8 +158,8 @@ public class LdapDAO {
 	public List<EmployeeLdap> getDivisionLeader(String divisionLeaderName, String division) {
 		logger.info("Getting Division Leaders from LDAP.");
         AndFilter andFilter = new AndFilter()
-                .and( new EqualsFilter( "displayName", divisionLeaderName ) )
-                .and( new EqualsFilter( "department", division ) );
+                .and(new EqualsFilter(propertyProvider.getLdapFieldForDisplayName(), divisionLeaderName))
+                .and( new EqualsFilter( propertyProvider.getLdapFieldForDivision(), division ) );
         logger.debug("LDAP Query {}", andFilter.encode());
         List search = ldapTemplate.search("", andFilter.encode(), new EmployeeAttributeMapper());
         logger.debug("LDAP Query finished. result.size = {}", search.size());
@@ -147,8 +169,10 @@ public class LdapDAO {
     @SuppressWarnings("unchecked")
     public List<Map> getDivisions() {
         AndFilter andFilter = new AndFilter()
-                .and(new EqualsFilter("objectClass", "group"))
-                .and(new LikeFilter("cn", "_Project Center *"));
+                .and(new EqualsFilter(
+                        propertyProvider.getLdapFieldForObjectClass(),
+                        propertyProvider.getLdapObjectClassDivision()))
+                .and(new LikeFilter("cn", propertyProvider.getLdapCnDivision()));
         SearchControls ctls = new SearchControls();
         ctls.setSearchScope(SearchControls.SUBTREE_SCOPE);
 
@@ -174,16 +198,16 @@ public class LdapDAO {
 			EmployeeLdap employee = new EmployeeLdap();
 
             employee.setObjectSid   (LdapUtils.convertBinarySidToString((byte[]) attributes.get(SID).get()));
-            employee.setDepartment  ( getAttributeByName( attributes, "department" ) );
-            employee.setDisplayName ( getAttributeByName( attributes, "displayName" ));
-            employee.setEmail       ( getAttributeByName( attributes, "mail"));
-		    employee.setManager     ( getAttributeByName( attributes, "manager" ) );
-            employee.setTitle       ( getAttributeByName( attributes, "title" ) );
-            employee.setWhenCreated ( getAttributeByName( attributes, "whenCreated" ) );
-            employee.setCity        ( getAttributeByName( attributes, "l" ) );
-            employee.setMailNickname( getAttributeByName( attributes, "mailNickname" ) );
+            employee.setDepartment  ( getAttributeByName( attributes, propertyProvider.getLdapFieldForDivision()));
+            employee.setDisplayName ( getAttributeByName( attributes, propertyProvider.getLdapFieldForDisplayName()));
+            employee.setEmail       ( getAttributeByName( attributes, propertyProvider.getLdapFieldForEmail()));
+		    employee.setManager     ( getAttributeByName( attributes, propertyProvider.getLdapFieldForManager()));
+            employee.setTitle       ( getAttributeByName( attributes, propertyProvider.getLdapFieldForTitle()));
+            employee.setWhenCreated ( getAttributeByName( attributes, propertyProvider.getLdapFieldForWhenCreated()));
+            employee.setCity        ( getAttributeByName( attributes, propertyProvider.getLdapFieldForCity()));
+            employee.setMailNickname( getAttributeByName( attributes, propertyProvider.getLdapFieldForMailNickname()));
 
-            Attribute ldapCn = attributes.get("distinguishedname");
+            Attribute ldapCn = attributes.get(propertyProvider.getLdapFieldForLdapCn());
 			if(ldapCn != null)
 				employee.setLdapCn(ldapCn.get().toString());
 			
