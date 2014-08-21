843c4fa8397cc9fc93eec596c229a8579bf2607d
abayanov <archy1188@yandex.ru>
2014-08-21 16:54:19 +0600

    Bug fix

--- src/main/java/com/aplana/timesheet/service/SendMailService.java
+++ src/main/java/com/aplana/timesheet/service/SendMailService.java
@@ -337,7 +337,10 @@ public class SendMailService {
                 model.put("errors", "Unexpected error: " + exception.getMessage());
                 // получим ФИО пользователя
                 String FIO = "<не определен>";
-                TimeSheetUser securityUser = securityService.getSecurityPrincipal();
+                TimeSheetUser securityUser = null;
+                try {
+                    securityUser = securityService.getSecurityPrincipal();
+                } catch (NullPointerException e) {}
                 if (securityUser != null) {
                     int employeeId = securityUser.getEmployee().getId();
                     FIO = employeeService.find(employeeId).getName();
